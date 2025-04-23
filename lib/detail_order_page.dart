4d3502c8dc117b8346c30025257f7356956b898a
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-04-23 15:46:40 +0700

    Membuat file detail_order_page dan membuat kelas DetailOrderPage sebagai StatelessWidget

---
+++ lib/detail_order_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DetailOrderPage extends StatelessWidget {
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+  
+  const DetailOrderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
